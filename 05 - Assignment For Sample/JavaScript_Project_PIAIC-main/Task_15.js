86020857bcc0b741932d7545e78f69517c97e05d
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-10-18 10:08:08 +0500

    update class

---
+++ 05 - Assignment For Sample/JavaScript_Project_PIAIC-main/Task_15.js
@@ -0,0 +1,20 @@
+var toInvite = ["Muhammad", "Ahsan", "Ali"];
+var cantCome = "Muhammad";
+
+//Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+console.log(cantCome + " can't come");
+
+//Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+toInvite.forEach((name, i) => {
+  if (name === cantCome) {
+    toInvite[i] = "Ahsan";
+
+    // Print a second set of invitation messages, one for each person who is still in your list.
+
+    toInvite.forEach((name) => {
+      console.log("Hi " + name + "!" + " I would like to invite you to dinner");
+    });
+  }
+});
---
+++ 05 - Assignment For Sample/JavaScript_Project_PIAIC-main/Task_6.js
@@ -0,0 +1,6 @@
+var person_name = "\t Muhammad  \n \t        Ahsan \n";
+console.log(person_name);
+
+//The trim() method removes whitespace from both sides of a string.
+
+console.log(person_name.trim());
