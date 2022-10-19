53a11027394c517b8f933e4b74ce4db260d4d8db
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-10-19 12:25:42 +0500

    update

---
+++ 01 - Onsite Classes/03 - Assignment And Tasks/Quarter I JavaScript Homework Coding Exercises/Task_14.js
@@ -0,0 +1,5 @@
+var toInvite = ["Muhammad", "Ahsan", "Ali", "Raza"];
+
+toInvite.forEach((name) => {
+    console.log("Hi "+name+"!"+ " I would like to invite you to dinner")
+})
---
+++ 04 - Coding Practice/04 - Javascript Books Practice/Chapter - 02/app.js
@@ -0,0 +1,15 @@
+let str1 = "JavaScript is fun!";
+let str2 = "JavaScript is fun!";
+console.log("These two strings are the same:", str1 === str2);
+
+
+let a = "ali"; // a  sis string  varable
+
+console.log(typeof a);
+
+a = Number(a); // convert into number type
+console.log(typeof a);
+
+a = Boolean(a); // convert into boolean tye
+
+console.log(a);
