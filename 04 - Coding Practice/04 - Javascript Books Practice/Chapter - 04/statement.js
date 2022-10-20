d0cc5c6777128346ff5197e93248d45e36dfb107
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-10-20 13:37:33 +0500

    update chapter practice

---
+++ 04 - Coding Practice/04 - Javascript Books Practice/Chapter - 04/statement.js
@@ -0,0 +1,35 @@
+// if Else Statement
+
+// let rain = false;
+// if (rain) {
+//   console.log("Taking my umbrella when I need to go outside");
+// } else {
+//   console.log("I can leave my umbrella at home");
+// }
+
+// let marks = prompt("Enter the Marks"); // if else
+
+// if (marks >= 90) {
+//   console.log("Your Grade is A+");
+// } else if (marks >= 80) {
+//   console.log("Your Grade is A");
+// } else if (marks >= 70) {
+//   console.log("Your Grade is B+");
+// } else if (marks >= 60) {
+//   console.log("Your grade is B");
+// } else if (marks >= 50) {
+//   console.log("Your Are Pass");
+// } else if (marks < 50) {
+//   console.log("You Are Fial");
+// } else {
+//   console.log("Enter the Correct Value");
+// }
+
+// Switch Statements
+
+let marks = prompt("enter your Marks");
+switch (marks) {
+  case 90:
+    console.log("Your Grade is A+");
+    break;
+}
