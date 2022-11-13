0ed4076b21ebacc3b257d5bfb2fff0980310f106
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-13 22:54:26 +0500

    Update function.js

--- 02 - Online Classes/03 -  Sir Danyal/01 - Online Class Practice/01 - Function/function.js
+++ 02 - Online Classes/03 -  Sir Danyal/01 - Online Class Practice/01 - Function/function.js
@@ -10,3 +10,6 @@ console.log(fullDate);
 console.log(date.getDate());
 console.log(date.getDay());
 console.log(date.getFullYear());
+console.log(date.getTime());
+console.log(date.getDay);
+console.log(date.getMonth);
