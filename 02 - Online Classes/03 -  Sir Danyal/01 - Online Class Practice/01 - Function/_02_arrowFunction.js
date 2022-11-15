005be67767d752619b400af70d5358045152db8e
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-15 18:52:42 +0500

    update class

--- 02 - Online Classes/03 -  Sir Danyal/01 - Online Class Practice/01 - Function/_02_arrowFunction.js
+++ 02 - Online Classes/03 -  Sir Danyal/01 - Online Class Practice/01 - Function/_02_arrowFunction.js
@@ -9,13 +9,35 @@ console.log(" 💚 Start Arrow Function Topic");
 
 // 😍  Use of this // Arrow function could not use in this
 
-let person = {
-  fName: "Muhammad",
-  lName: "Ahsan",
-  age: 23,
-  fullName: function () {
-    console.log(this.fName + " " + this.lName);
-  },
+// let person = {
+//   fName: "Muhammad",
+//   lName: "Ahsan",
+//   age: 23,
+//   fullName: function () {
+//     console.log(this.fName + " " + this.lName);
+//   },
+// };
+
+// console.log(person.fullName());
+
+//  😍 Spread Operators
+
+// const numbersOne = [1, 2, 3];
+
+// const numbersTwo = [4, 5, 6];
+// const numbersCombined = [...numbersOne, ...numbersTwo];
+// console.log(numbersCombined);
+
+// 😍 Rest ParaMeeter
+
+const addUnlimitedNumbers = (...numbers) => {
+  let result = 0;
+  for (i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+    result = result + numbers[i];
+  }
+  return result;
 };
 
-console.log(person.fullName());
+const finalResult = addUnlimitedNumbers(0, 21, 13, 415, 12.1, 112);
+console.log(finalResult);
