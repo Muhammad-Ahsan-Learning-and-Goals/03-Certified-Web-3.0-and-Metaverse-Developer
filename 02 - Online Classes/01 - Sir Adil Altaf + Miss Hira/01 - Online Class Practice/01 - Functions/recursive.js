dcdfbdda3acc7e3419f8fa753e6ace7b9cc576a0
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-16 16:31:48 +0500

    update

--- 02 - Online Classes/01 - Sir Adil Altaf + Miss Hira/01 - Online Class Practice/01 - Functions/recursive.js
+++ 02 - Online Classes/01 - Sir Adil Altaf + Miss Hira/01 - Online Class Practice/01 - Functions/recursive.js
@@ -1,2 +1,12 @@
-// 💛 Recursive Function
-console.log("Helllo ");
+// Recursive Function
+
+function recursive(nr) {
+  console.log(" Started Function : ", nr);
+  if (nr > 0) {
+    recursive(--nr);
+  } else {
+    console.log("Recursion Completed ");
+  }
+  console.log("Function Ended : ", nr);
+}
+recursive(5);
