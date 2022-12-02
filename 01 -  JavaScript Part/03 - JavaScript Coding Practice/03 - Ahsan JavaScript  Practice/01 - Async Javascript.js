4630a5ccade55d7855a079c98db99e07f409fa5c
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-12-02 11:48:56 +0500

    update class

--- 01 -  JavaScript Part/03 - JavaScript Coding Practice/03 - Ahsan JavaScript  Practice/01 - Async Javascript.js
+++
@@ -1,8 +0,0 @@
-console.log(1);
-console.log(2);
-console.log(3);
-setTimeout(() => {
-  console.log("Call Back");
-}, 3000);
-
-console.log(4);
--- 02 -  TypeScript Part/03 - JavaScript Coding Practice/03 - Ahsan TypeScript Practice/01 - Learn TypeScript – Full Tutorial/01 - Learn TypeScript – Full Tutorial/basic.ts
+++
@@ -1,8 +0,0 @@
-
-
-let a = "2";
-let b = 43;
-let c = a+b ;
-
-console.log("The sub of a + b is : " ,  c )
-
