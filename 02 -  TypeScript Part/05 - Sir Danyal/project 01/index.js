7652d54216b61abc3dfb20e1080c4602926c5d9f
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-12-05 23:21:18 +0500

    update

---
+++ 02 -  TypeScript Part/05 - Sir Danyal/project 01/index.js
@@ -0,0 +1,5 @@
+var a = Math.random() > 0.6 ? "Greater" : "Less";
+console.log(a);
+var b = "Ahan";
+// b.length();
+console.log(b);
---
+++ 02 -  TypeScript Part/05 - Sir Danyal/project 01/index.ts
@@ -0,0 +1,6 @@
+let a = Math.random()>0.6 ? "Greater": "Less";
+console.log(a);
+
+let b="Ahan";
+// b.length();
+console.log(b);
