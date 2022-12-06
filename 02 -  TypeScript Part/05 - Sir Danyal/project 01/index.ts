e1cdf3d8674538ea654e48fd71d9c14e49e6cb5e
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-12-06 11:39:55 +0500

    up

--- 02 -  TypeScript Part/05 - Sir Danyal/project 01/index.ts
+++ 02 -  TypeScript Part/05 - Sir Danyal/project 01/index.ts
@@ -7,3 +7,15 @@ let b="Ahan";
 // because length is string property not functiion
 console.log(b);
 
+// implicit and Explicit (phly sy assign)
+
+// Type Annotation
+
+let a1= "Ahsan";
+console.log(a1.toUpperCase());
+
+
+
+let a2= 23.6;
+a2.toFixed();
+console.log(a2)
